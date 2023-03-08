f77132f5df620ae050131915b16066ceed36d9fe
kuldeepSingh <singhkuldeep7850@gmail.com>
2023-03-08 17:41:11 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { themeSettings } from "./theme";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Dashboard from './pages/dashboard'
+import Layout from "./pages/layout";
+import Products from "./pages/products"
+import Customers from './pages/customers'
+import Transactions from './pages/transactions'
+import Overview from './pages/overview'
+import Daily from "./pages/daily";
+import Monthly from "./pages/monthly";
+import Breakdown from "./pages/breakdown" 
+import Admin from './pages/admin'
+
+
+function App() {
+
+  const mode = useSelector((state) => state.global.mode)
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route element={<Layout/>}>
+              <Route path="/" element={<Navigate to="/dashboard" replace/>}/>
+              <Route path="/dashboard" element={<Dashboard/>} />
+              <Route path="/products" element={<Products/>} />
+              <Route path="/customers" element={<Customers/>} />
+              <Route path="/transactions" element={<Transactions/>} />
+              <Route path="/overview" element={<Overview/>} />
+              <Route path="/daily" element={<Daily/>} />
+              <Route path="/monthly" element={<Monthly/>} />
+              <Route path="/breakdown" element={<Breakdown/>} />
+              <Route path="/admin" element={<Admin/>} />
+            
+            </Route>
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
